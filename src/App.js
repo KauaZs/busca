54db377b42d74de309cabe3c52fa13157802f19b
Kauã <kauagamerxp7@gmail.com>
2023-07-07 08:39:35 -0300

    Initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
 
+import api from './services/api';
+import './styles.css';
 function App() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+  async function clickedSearch() {
+    if(!input) return;
+    try {
+      const res = await api.get(`${input}/json`)
+      setCep(res.data)
+      setInput('')
+    } catch {
+      alert('CEP Invalido')
+      setInput('')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='title'>Busca CEP</h1>
+
+    <div className='input'>
+      <input 
+      type='text' 
+      placeholder='Digite o cep xxxx-xxx'
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      />
+
+      <button className='buttonSrc'
+      onClick={clickedSearch}> 
+      <BsSearch 
+      color="#FFF"
+      size={25}
+      /></button>
+      
+    </div>
+    
+    {Object.keys(cep).length > 0 && (
+       <main className='main'>
+       <h2>CEP Buscado: {cep.cep}</h2>
+ 
+       <span>Rua: {cep.logradouro} </span>
+       <span>Complemento: {cep.complemento}</span>
+       <span>{cep.bairro}</span>
+       <span>{cep.localidade} - {cep.uf}</span>
+     </main>
+    )}
+
     </div>
   );
 }
